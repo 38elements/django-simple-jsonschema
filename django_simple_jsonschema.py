fac6fbedab02f1069bd745b23b641179c87c5bb9
38elements <mh19820223@gmail.com>
2015-12-19 11:26:08 +0900

    add django_simple_jsonschema.py

---
+++ django_simple_jsonschema.py
@@ -0,0 +1,13 @@
+# from django.conf import settings
+
+
+class SimpleJsonschemaMiddleware(object):
+
+    def __init__(self):
+        pass
+
+    def process_request(request):
+        pass
+
+    def process_exception(request, exception):
+        pass
