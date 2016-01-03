90e3ba446b8a6a8ba3b255e03aed1dc048e4ff1a
38elements <mh19820223@gmail.com>
2016-01-03 17:29:54 +0900

    Fix setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-simple-jsonschema',
-    version='0.0.2',
+    version='0.0.3',
     author='38elements',
     description='django-simple-jsonschema is middleware for integrating django and jsonschema. ',
     license='MIT License',
@@ -12,12 +12,12 @@ setup(
         'jsonschema>=2.5'
     ],
     py_modules=['django_simple_jsonschema'],
-    keywords='django jsonschema',
-    classifiers=(
+    keywords=['django', 'jsonschema'],
+    classifiers=[
         'Development Status :: 3 - Alpha',
         'Framework :: Django',
         'Programming Language :: Python :: 3 :: Only',
         'License :: OSI Approved :: MIT License',
         'Intended Audience :: Developers'
-    )
+    ]
 )
